ddd92d4f8fc4f4b7658ff869af725b6a4a17fac1
artemlomov56-del <artemlomov56@gmail.com>
2026-06-27 23:58:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -93,4 +93,6 @@ if __name__ == '__main__':
     print("✅ САЙТ ЗАПУЩЕН!")
     print("📱 Открой в браузере: http://localhost:5000")
     print("🔐 Админка: http://localhost:5000/admin")
-    app.run(debug=True, port=5000)
+ import os
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
